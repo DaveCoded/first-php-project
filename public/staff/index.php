82179dac6932295d97939877299a9c6022d39690
David <davidbernhard@live.com>
2018-08-15 15:51:57 +0100

    Create file structure

---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
+
+    <nav>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
+
+    <footer>
+      &copy; 2018 Globe Bank
+    </footer>
+
+  </body>
+</html>
