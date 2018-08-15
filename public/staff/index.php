15990c17a3379b78110d670ebd11623b4a683b53
David <davidbernhard@live.com>
2018-08-15 17:00:13 +0100

    Restructure into header and footer files

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,31 +1,8 @@
 <?php require_once('../../private/initialize.php'); ?>
 
-<!doctype html>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
-  </head>
+  <div id="content">
+  </div>
 
-  <body>
-    <header>
-      <h1>GBI Staff Area</h1>
-    </header>
-
-    <nav>
-      <ul>
-        <li><a href="index.php">Menu</a></li>
-      </ul>
-    </nav>
-
-    <div id="content">
-    </div>
-
-    <footer>
-      &copy; <?php echo date('Y'); ?> Globe Bank
-    </footer>
-
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
