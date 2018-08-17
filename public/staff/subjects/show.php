76b96afe9984b1ff8ec91ffeb1adc743add51432
David <davidbernhard@live.com>
2018-08-17 11:01:28 +0100

    Add links from snippet

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,3 +4,7 @@
   echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
